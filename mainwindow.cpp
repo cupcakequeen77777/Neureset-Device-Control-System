768584e8ddaa8e5d019c9481a152802d42e46c09
cupcakequeen77777 <leeanneking@cmail.carleton.ca>
2024-03-26 14:52:53 -0400

    Added button slots

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,42 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_pauseTreatement_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_continueTreatment_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_stopTreatement_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_disconnectSite_clicked()
+{
+
+}
+
+
+
+
+
+void MainWindow::on_widget_menuOpts_itemClicked(QListWidgetItem *item)
+{
+
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QListWidgetItem>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,6 +15,20 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_btn_pauseTreatement_clicked();
+
+    void on_btn_continueTreatment_clicked();
+
+    void on_btn_stopTreatement_clicked();
+
+    void on_btn_disconnectSite_clicked();
+
+
+    void on_widget_menuOpts_itemClicked(QListWidgetItem *item);
 
 private:
     Ui::MainWindow *ui;
