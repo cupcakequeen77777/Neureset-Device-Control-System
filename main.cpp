19193c8f60dccd50f39c6501b9796e31c9b296c4
saging <iamemrehansagin09@gmail.com>
2024-03-27 12:58:06 -0400

    Emrehan test commit

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main(int argc, char *argv[]){
     cout <<"Hello World!";
     cout <<"LeeAnne Here";
     cout <<endl<<"Sara Here"<<endl;
+    cout << "Emrehan test!" << endl;
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
